5e8b84fad956a46d65efab71037cfcff2b8221c7
DevSuman <dev.suman.me@gmail.com>
2024-03-21 11:19:26 +0530

    Updates

---
+++ script1.js
@@ -0,0 +1,17 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+  }
+  
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+  
+  function calculate() {
+    let result;
+    try {
+      result = eval(document.getElementById('display').value);
+      document.getElementById('display').value = result;
+    } catch (error) {
+      document.getElementById('display').value = 'Error';
+    }
+  }
